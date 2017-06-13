03bae6d743da94289d6eaf013a70f6e4e6687c37
Hiroki Honda <hon87kot12.xp@gmail.com>
2017-06-13 05:11:15 -0700

    Fixed base layout for List up records view controller

--- ExpenseManager/ExpenseManager/ListUpRecordsViewController.swift
+++ ExpenseManager/ExpenseManager/ListUpRecordsViewController.swift
@@ -3,17 +3,32 @@ import FSCalendar
 
 class ListUpRecordsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FSCalendarDataSource, FSCalendarDelegate, UIGestureRecognizerDelegate {
     
+    var expenses: Array<Expense> = []
+    
     @IBOutlet weak var calendar: FSCalendar!
-//    @IBOutlet weak var tableView: UITableView!
 //    @IBOutlet weak var animationSwitch: UISwitch!
-    
-//    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
-    
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var modeToggle: UISegmentedControl!
+    @IBAction func modeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.calendar.setScope(.week, animated: true)
+        case 1:
+            self.calendar.setScope(.month, animated: true)
+        default:
+            break;
+        }
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+
     fileprivate lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy/MM/dd"
         return formatter
     }()
+
     fileprivate lazy var scopeGesture: UIPanGestureRecognizer = {
         [unowned self] in
         let panGesture = UIPanGestureRecognizer(target: self.calendar, action: #selector(self.calendar.handleScopeGesture(_:)))
@@ -21,25 +36,12 @@ class ListUpRecordsViewController: UIViewController, UITableViewDataSource, UITa
         panGesture.minimumNumberOfTouches = 1
         panGesture.maximumNumberOfTouches = 2
         return panGesture
-        }()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        if UIDevice.current.model.hasPrefix("iPad") {
-//            self.calendarHeightConstraint.constant = 400
-//        }
-        
-        self.calendar.select(Date())
-        print("### aaa")
-        
-        self.view.addGestureRecognizer(self.scopeGesture)
-//        self.tableView.panGestureRecognizer.require(toFail: self.scopeGesture)
-        self.calendar.scope = .week
-        
-        // For UITest
-        self.calendar.accessibilityIdentifier = "calendar"
-        
+        initializeData()
+        initializeView()
     }
     
     deinit {
@@ -49,22 +51,22 @@ class ListUpRecordsViewController: UIViewController, UITableViewDataSource, UITa
     // MARK:- UIGestureRecognizerDelegate
     
     func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        let shouldBegin = self.tableView.contentOffset.y <= -self.tableView.contentInset.top
-//        if shouldBegin {
-//            let velocity = self.scopeGesture.velocity(in: self.view)
-//            switch self.calendar.scope {
-//            case .month:
-//                return velocity.y < 0
-//            case .week:
-//                return velocity.y > 0
-//            }
-//        }
-//        return shouldBegin
-        return true
+        print("gestureRecognizerShouldBegin")
+        let shouldBegin = self.tableView.contentOffset.y <= -self.tableView.contentInset.top
+        if shouldBegin {
+            let velocity = self.scopeGesture.velocity(in: self.view)
+            switch self.calendar.scope {
+            case .month:
+                return velocity.y < 0
+            case .week:
+                return velocity.y > 0
+            }
+        }
+        return shouldBegin
     }
     
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-//        self.calendarHeightConstraint.constant = bounds.height
+        self.calendarHeightConstraint.constant = bounds.height
         self.view.layoutIfNeeded()
     }
     
@@ -87,43 +89,91 @@ class ListUpRecordsViewController: UIViewController, UITableViewDataSource, UITa
         return 2
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return [2,20][section]
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let identifier = ["cell_month", "cell_week"][indexPath.row]
-            let cell = tableView.dequeueReusableCell(withIdentifier: identifier)!
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-            return cell
-        }
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return [2,20][section]
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if indexPath.section == 0 {
+//            let identifier = ["cell_month", "cell_week"][indexPath.row]
+//            let cell = tableView.dequeueReusableCell(withIdentifier: identifier)!
+//            return cell
+//        } else {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+//            return cell
+//        }
+//    }
     
     
     // MARK:- UITableViewDelegate
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        if indexPath.section == 0 {
-//            let scope: FSCalendarScope = (indexPath.row == 0) ? .month : .week
-//            self.calendar.setScope(scope, animated: self.animationSwitch.isOn)
+
+}
+
+/**
+ * Initializer
+ */
+extension ListUpRecordsViewController {
+    func initializeData() {
+        let expenseDao = ExpenseDao()
+        expenses = Array(expenseDao.findAllExpenses())
+    }
+    
+    func initializeView() {
+        
+        if UIDevice.current.model.hasPrefix("iPad") {
+            self.calendarHeightConstraint.constant = 400
+        }
+        
+        self.calendar.select(Date())
+        print("### aaa")
+        
+        self.view.addGestureRecognizer(self.scopeGesture)
+        self.tableView.panGestureRecognizer.require(toFail: self.scopeGesture)
+        self.calendar.scope = .week
+        
+        // For UITest
+        self.calendar.accessibilityIdentifier = "calendar"
+        
+        // set interfaces
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+/**
+ * Implementation methods for table view
+ */
+extension ListUpRecordsViewController {
+    // getView
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO recycle
+        if let tableCell = tableView.cellForRow(at: indexPath) {
+            print("recycle \(indexPath.row)")
+            return tableCell
+        } else {
+            print("new \(indexPath.row)")
+            let newTableCell = UITableViewCell(style: .default, reuseIdentifier: "row")
+            let expense = expenses[indexPath.row]
+            newTableCell.textLabel?.text = "\(expense.id) / \(expense.type?.name ?? "none") / \(expense.date.description)"
+            return newTableCell
         }
     }
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 10
+    // getCount
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expenses.count
     }
     
-    // MARK:- Target actions
-    
-    @IBAction func toggleClicked(sender: AnyObject) {
-//        if self.calendar.scope == .month {
-//            self.calendar.setScope(.week, animated: self.animationSwitch.isOn)
-//        } else {
-//            self.calendar.setScope(.month, animated: self.animationSwitch.isOn)
-//        }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.section == 0 {
+            let scope: FSCalendarScope = (indexPath.row == 0) ? .month : .week
+            self.calendar.setScope(scope, animated: true)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
     }
 }
